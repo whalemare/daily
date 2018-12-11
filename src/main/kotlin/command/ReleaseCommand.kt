765d554c9575ff494c3e2ee16fb72fc93efa003a
whalemare <nanostet@gmail.com>
2018-12-11 17:50:40 +0700

    Add options to commit provider

---
+++ src/main/kotlin/command/ReleaseCommand.kt
@@ -0,0 +1,11 @@
+package command
+
+/**
+ * @since 2018
+ * @author Anton Vlasov - whalemare
+ */
+class ReleaseCommand : Runnable {
+    override fun run() {
+
+    }
+}
--- src/main/kotlin/commit/RawCommitProvider.kt
+++ src/main/kotlin/commit/RawCommitProvider.kt
@@ -7,12 +7,39 @@ import org.zeroturnaround.exec.ProcessExecutor
  * @since 2018
  * @author Anton Vlasov - whalemare
  */
-class RawCommitProvider : Provider<String> {
+class RawCommitProvider(val since: Since = Since.Raw("6am")) : Provider<String> {
+
+    sealed class Since {
+
+        /**
+         * TODO: rewrite to windows
+         * Works only on Unix
+         */
+        class LatestTag() : Tag("\$(git describe --tags --abbrev=0 @^)")
+
+        open class Tag(tag: String) : Raw("$tag..HEAD")
+
+        /**
+         * Expected some raw result that will be putted
+         * ```bash
+         * git log --since='PARAM'
+         * ```
+         * where PARAM it is [text] param
+         * @param text 6am, 9pm, c3eeab6..0158d99, tag-name..HEAD, today
+         */
+        open class Raw(val text: String) : Since()
+    }
 
     override fun provide(): String {
+        val since = when(since) {
+            is Since.Raw -> {
+                "--since='${since.text}'"
+            }
+        }
+
         val rawText = ProcessExecutor().command(
             "git", "log",
-            "--since='6am'",
+            since,
             "--pretty=medium",
             "--date=iso-strict"
         ).readOutput(true)
